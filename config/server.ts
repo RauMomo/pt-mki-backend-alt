fc66930f4804e6496c68bb7a92600d81f0b3fb22
muhammadrizqiko <muhammadrizqiko@rocketmail.com>
2024-12-16 14:26:55 +0700

    fix: config files

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,17 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "SYk9J4yrk9bmsjofo4vGqA",
+      "TeBKqubQBpoDT0shtZwAg",
+    ]),
   },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  keys: env.array("APP_KEYS", [
+    "SYk9J4yrk9bmsjofo4vGqA",
+    "TeBKqubQBpoDT0shtZwAg",
+  ]),
 });
