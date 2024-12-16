28453d532b3a309728c5def79f225b78d3f779d4
muhammadrizqiko <muhammadrizqiko@rocketmail.com>
2024-12-16 14:53:28 +0700

    fix ssl ca certificates

--- config/server.ts
+++ config/server.ts
@@ -1,17 +1,23 @@
-export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1338),
-  app: {
+const sslRootCas = require("ssl-root-cas/latest").create();
+
+module.exports = ({ env }) => {
+  require('https').globalAgent.options.ca = sslRootCas;
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1338),
+    app: {
+      keys: env.array("APP_KEYS", [
+        "SYk9J4yrk9bmsjofo4vGqA",
+        "TeBKqubQBpoDT0shtZwAg",
+      ]),
+    },
+    webhooks: {
+      populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    },
     keys: env.array("APP_KEYS", [
       "SYk9J4yrk9bmsjofo4vGqA",
       "TeBKqubQBpoDT0shtZwAg",
     ]),
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-  keys: env.array("APP_KEYS", [
-    "SYk9J4yrk9bmsjofo4vGqA",
-    "TeBKqubQBpoDT0shtZwAg",
-  ]),
-});
+  }
+};
